aa94f3b2eccc970584e4ed910b6e15e59ff555a6
Dikshita621 <dikshitachoudhary845@gmail.com>
2026-07-14 19:02:03 +0530

    Add application header

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
 import customtkinter as ctk
 
-#appearance mode
-ctk.set_appearance_mode("System")  # "Light", "Dark", or "System"
-#default color theme
+# App Configuration
+
+ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("blue")
 
-#the main window
+
+# Main Window
+
 app = ctk.CTk()
 
-# Window title
 app.title("Secure Password Toolkit")
 
-# Window size
-app.geometry("900x650")
+WINDOW_WIDTH = 900
+WINDOW_HEIGHT = 650
 
-# Prevent resizing
+app.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
 app.resizable(False, False)
 
-# Run the application
+
+# Header
+
+title = ctk.CTkLabel(
+    app,
+    text="🔐 Secure Password Toolkit",
+    font=("Arial", 28, "bold")
+)
+
+title.pack(pady=(20, 10))
+
+subtitle = ctk.CTkLabel(
+    app,
+    text="Generate secure passwords and check their strength",
+    font=("Arial", 14)
+)
+
+subtitle.pack()
+
+# Run
+
 app.mainloop()
